930c0076599b16d19bc80b95b1497ea521bb8ce2
JohnCselcuk <85411967+JohnCselcuk@users.noreply.github.com>
2021-11-28 09:57:24 -0500

    Add files via upload

---
+++ js/logic.js
@@ -0,0 +1,27 @@
+// We create the map object with options.
+let map = L.map('mapid').setview([36.1733, -120.1794], 7);
+
+// Coordinates for each point to be used in the polyline
+let line = [
+ [33.9416, -118.4085],
+ [37.6213, -122.3790]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+}).addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+  // Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
+ // Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
